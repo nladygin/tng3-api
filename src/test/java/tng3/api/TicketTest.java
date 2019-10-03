cba243846f77d9cc944bf637e6c2e9a6599a9652
nikolay ladygin <nladygin@hrsinternational.com>
2019-10-03 11:07:20 +0500

    Ticket tests fixed

--- src/test/java/tng3/api/TicketTest.java
+++ src/test/java/tng3/api/TicketTest.java
@@ -3,14 +3,8 @@ package tng3.api;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import tng3.api.entity.APIResponse;
-import tng3.api.entity.Account;
-import tng3.api.entity.Ticket;
-
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -19,20 +13,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@PropertySource({"data.properties"})
 public class TicketTest extends BaseTest {
 
-    @Autowired
-    private Ticket ticket;
-
-    @Autowired
-    private Utils utils;
-
-    @Value("${emplMagstripe}")
-    private String emplMagstripe;
-
-
     private final String endpoint = "/tickets";
+
+
 
 
     @Test
@@ -54,10 +39,53 @@ public class TicketTest extends BaseTest {
                     ).get("number");
 
                         HashMap<String, String> additional = new HashMap<>();
-                        additional.put("empl", emplMagstripe);
+                            additional.put("empl", config.emplMagstripe);
 
-                            response = utils.go(endpoint + "/replace/" + originalCode + "/" + originalCode + "01", Method.POST, null, additional);
+                            response = utils.go(endpoint + "/replace/" + originalCode + "/" + utils.generateDigits(8), Method.POST, null, additional);
                             assertThat(response.getSuccess(), equalTo(true));
     }
 
+
+    @Test
+    public void replaceNonexistentTicket(){
+        HashMap<String, String> additional = new HashMap<>();
+            additional.put("empl", config.emplMagstripe);
+
+                APIResponse response = utils.go(endpoint + "/replace/" + "666" + "/" + utils.generateDigits(8), Method.POST, null, additional);
+                assertThat(response.getSuccess(), equalTo(false));
+                assertThat(utils.getErrorCode(response.getError()), equalTo(10));
+    }
+
+
+    @Test
+    public void replaceTicketWithBadEmployee(){
+        APIResponse response = utils.go(endpoint, Method.GET);
+        assertThat(response.getSuccess(), equalTo(true));
+
+            String originalCode = (String) (
+                    (HashMap<String, Object>)
+                            ((ArrayList)
+                                    (response.getPayload())).get(0)
+            ).get("number");
+
+                HashMap<String, String> additional = new HashMap<>();
+                    additional.put("empl", utils.generateString(10));
+
+        response = utils.go(endpoint + "/replace/" + originalCode + "/" + utils.generateDigits(8), Method.POST, null, additional);
+        assertThat(response.getSuccess(), equalTo(false));
+        assertThat(utils.getErrorCode(response.getError()), equalTo(10));
+    }
+
+
+
+
+
+
+
+
+    @Autowired
+    private Utils utils;
+
+    @Autowired
+    private Config config;
 }
--- src/test/java/tng3/api/entity/Ticket.java
+++ src/test/java/tng3/api/entity/Ticket.java
@@ -1,13 +1,14 @@
 package tng3.api.entity;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import tng3.api.Utils;
 
-@Configuration
-@ComponentScan("tng3.api")
-@PropertySource({"data.properties"})
+import java.io.IOException;
+
 public class Ticket implements Entity {
 
     public int number;
@@ -16,11 +17,27 @@ public class Ticket implements Entity {
 
 
 
-    @Override
-    public String asJsonString() {
-        return null;
-    }
-
     public Ticket(){}
 
+
+
+    @Override
+    public String asJsonString() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        String json = null;
+        try {
+            json = mapper.writeValueAsString(this);
+        } catch (IOException e) {
+            log.error(e.getStackTrace());
+        }
+        return json;
+    }
+
+
+
+
+
+    private Utils utils = new Utils();
+    private final Logger log = LogManager.getLogger();
 }
