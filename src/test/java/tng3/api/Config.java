b77ee8969440c50e0fa80fbd6f0e3f7260133537
nikolay ladygin <nladygin@hrsinternational.com>
2019-10-04 13:31:13 +0500

    Config refactor

--- src/test/java/tng3/api/Config.java
+++ src/test/java/tng3/api/Config.java
@@ -18,38 +18,4 @@ public class Config {
     @Value("${therapist_id}")   public String therapistID;
     @Value("${tender_id}")      public Integer tenderID;
 
-
-
-
-//    @Value("${doc.offset}")     public String docOffset;
-//    @Value("${doc.count}")      public String docCount;
-//    @Value("${doc.account}")    public String docAccount;
-
-//    @Value("${offer.offset}")       public String offerOffset;
-//    @Value("${offer.count}")        public String offerCount;
-//    @Value("${offer.id}")           public String offerID;
-//    @Value("${offer.therapist_id}") public String offerTherapistID;
-
-//    @Value("${booking.therapist_id}")   public String bookingTherapistID;
-//    @Value("${booking.offer_id}")       public String bookingOfferID;
-
-//    @Value("${bill.outletID}")       public Integer billOutletID;
-//    @Value("${bill.item.offerID}")   public Integer billItemOfferID;
-//    @Value("${bill.item.name}")      public String billItemName;
-//    @Value("${bill.item.count}")     public Integer billItemCount;
-//    @Value("${bill.item.amount}")    public Double billItemAmount;
-//    @Value("${bill.item.discount}")  public Double billItemDiscount;
-//    @Value("${bill.item.bookingID}") public Integer billItemBookingID;
-//    @Value("${bill.item.id}")        public Integer billItemID;
-//    @Value("${bill.item.reference}") public String billItemReference;
-
-//    @Value("${bill.payment.tenderID}")  public Integer billPaymentTenderID;
-//    @Value("${bill.payment.name}")      public String billPaymentName;
-//    @Value("${bill.payment.amount}")    public Double billPaymentAmount;
-//    @Value("${bill.payment.reference}") public String billPaymentReference;
-//    @Value("${bill.payment.exUid}")     public String billPaymentExUid;
-//    @Value("${bill.payment.account}")   public String billPaymentAccount;
-//    @Value("${bill.payment.voucher}")   public String billPaymentVoucher;
-
-
 }
