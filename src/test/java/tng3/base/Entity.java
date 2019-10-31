f1687ee0c34c7dbf6a79b4e1294c9ac85110387f
nikolay ladygin <nladygin@hrsinternational.com>
2019-10-31 12:28:59 +0500

    staff

--- src/test/java/tng3/base/Entity.java
+++ src/test/java/tng3/base/Entity.java
@@ -1,7 +1,23 @@
 package tng3.base;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.apache.logging.log4j.LogManager;
+
+import java.io.IOException;
+
 public interface Entity {
 
-    String asJsonString();
+    default String asJsonString() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        String json = null;
+        try {
+            json = mapper.writeValueAsString(this);
+        } catch (IOException e) {
+            LogManager.getLogger().error(e.getStackTrace());
+        }
+        return json;
+    }
 }
---
+++ src/test/java/tng3/staffapi/config/Config.java
@@ -0,0 +1,20 @@
+package tng3.staffapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("tng3.staffapi, tng3.helper")
+@PropertySource({"config.properties"})
+public class Config {
+
+    @Value("${server.url}")     public String serverURL;
+    @Value("${staff_app_id}")   public String appID;
+
+
+
+
+    public Config(){}
+}
