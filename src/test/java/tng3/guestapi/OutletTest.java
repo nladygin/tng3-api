44b860a52d71b3f0a1e2f2803cd6c8eb0374ba5e
nikolay ladygin <nladygin@hrsinternational.com>
2019-11-08 13:42:04 +0500

    outlet tests

---
+++ src/test/java/tng3/guestapi/OutletTest.java
@@ -0,0 +1,29 @@
+package tng3.guestapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import tng3.base.APIResponse;
+import tng3.entity.Outlet;
+import tng3.guestapi.action.AccountAction;
+import tng3.guestapi.action.OutletAction;
+import tng3.guestapi.entity.Accounts;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class OutletTest extends BaseTest {
+
+
+
+    @Test
+    public void getOutlets() {
+        APIResponse response = outletAction.getOutlets();
+        outletAction.checkResponseSuccess(response, true);
+        outletAction.validateResponsePayload(response, Outlet.class, true);
+    }
+
+
+
+    @Autowired private OutletAction outletAction;
+}
---
+++ src/test/java/tng3/guestapi/action/OutletAction.java
@@ -0,0 +1,20 @@
+package tng3.guestapi.action;
+
+import io.restassured.http.Method;
+import org.springframework.stereotype.Component;
+import tng3.base.APIResponse;
+import tng3.base.Action;
+
+@Component
+public class OutletAction extends Action {
+
+    private final String endpoint = "/api/outlets";
+
+
+
+    public APIResponse getOutlets(){
+        return requestHelper.go(endpoint, Method.GET, null, null);
+    }
+
+
+}
