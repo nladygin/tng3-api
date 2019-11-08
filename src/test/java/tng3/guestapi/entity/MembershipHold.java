6d7eee0a5a53c6336f22c291f05961d1d9306916
nikolay ladygin <nladygin@hrsinternational.com>
2019-11-08 17:58:28 +0500

    membership tests

---
+++ src/test/java/tng3/guestapi/entity/MembershipHold.java
@@ -0,0 +1,14 @@
+package tng3.guestapi.entity;
+
+import tng3.base.Entity;
+
+import java.util.List;
+
+
+public class MembershipHold implements Entity {
+
+    public String start;
+    public String end;
+    public String notes;
+
+}
