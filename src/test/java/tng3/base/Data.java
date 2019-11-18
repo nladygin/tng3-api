0da14999178884058a53ba29fab8d4e62d47d007
nikolay ladygin <nladygin@hrsinternational.com>
2019-11-18 15:31:51 +0500

    test data optimization

--- src/test/java/tng3/base/Data.java
+++ src/test/java/tng3/base/Data.java
@@ -10,27 +10,31 @@ import org.springframework.stereotype.Component;
 @PropertySource({"data.properties"})
 public class Data {
 
-    @Value("${offer_id}")           public int offerID;
-    @Value("${card_id}")            public int cardID;
     @Value("${outlet_id}")          public int outletID;
+    @Value("${offset}")             public int offset;
+    @Value("${count}")              public int count;
+
+    @Value("${card_id}")            public int cardID;
     @Value("${card_magstripe}")     public String cardMagstripe;
+    @Value("${card_email}")         public String cardEmail;
+    @Value("${card_phone}")         public String cardPhone;
+    @Value("${card_password}")      public String cardPassword;
+    @Value("${card_type}")          public String cardType;
+
+    @Value("${therapist_id}")       public int therapistID;
     @Value("${empl_magstripe}")     public String emplMagstripe;
     @Value("${empl_username}")      public String emplUsername;
     @Value("${empl_password}")      public String emplPassword;
-    @Value("${therapist_id}")       public int therapistID;
+
     @Value("${spa_package_id}")     public int spaPackageID;
-    @Value("${card_password}")      public String cardPassword;
-    @Value("${card_email}")         public String cardEmail;
-    @Value("${card_type}")          public String cardType;
-    @Value("${card_phone}")         public String cardPhone;
+    @Value("${offer_id}")           public int offerID;
     @Value("${class_id}")           public int classID;
-    @Value("${offset}")             public int offset;
-    @Value("${count}")              public int count;
+    @Value("${voucher_num}")        public String voucherNum;
+
     @Value("${tender_id}")          public int tenderID;
     @Value("${tender_name}")        public String tenderName;
     @Value("${deposit_tender_id}")  public int depositTenderID;
     @Value("${deposit_tender_name}")public String depositTenderName;
-    @Value("${voucher_num}")        public String voucherNum;
 
 
 
