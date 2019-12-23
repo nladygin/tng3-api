56721810ca31b3e16d7b31c3ca57af22ba2d9c26
nikolay ladygin <nladygin@hrsinternational.com>
2019-12-23 09:17:24 +0500

    ignore pincode test via sms

--- src/test/java/tng3/guestapi/PinCodeTest.java
+++ src/test/java/tng3/guestapi/PinCodeTest.java
@@ -1,5 +1,6 @@
 package tng3.guestapi;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,7 @@ public class PinCodeTest extends BaseTest {
     }
 
 
+    @Ignore
     @Test
     public void requestPinCodeByPhone() {
         PinCodeRequest pinCodeRequest = new PinCodeRequest(null, data.cardPhone);
