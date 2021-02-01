edb4d2124d42856e3973afe322d796c33aa671e0
nikolay ladygin <nladygin@hrsinternational.com>
2021-02-01 10:00:21 +0500

    fixed ticket tests

--- src/test/java/tng3/guestapi/OfferTest.java
+++ src/test/java/tng3/guestapi/OfferTest.java
@@ -257,7 +257,7 @@ public class OfferTest extends BaseTest {
         );
         offerAction.checkResponseSuccess(response, true);
         offerAction.validateResponsePayload(response, Offers.class, false);
-        offerAction.checkOnOffersNumber(response, 5);
+        offerAction.checkOnOffersNumber(response, 4);
     }
 
 
@@ -286,7 +286,7 @@ public class OfferTest extends BaseTest {
         );
         offerAction.checkResponseSuccess(response, true);
         offerAction.validateResponsePayload(response, Offers.class, false);
-        offerAction.checkOnOffersNumber(response, 5);
+        offerAction.checkOnOffersNumber(response, 4);
     }
 
 
